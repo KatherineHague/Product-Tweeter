dc0c07a2948b487bf436b0e9cc038e08660f76f2
Katherine Hague <kat@Princess.local>
2011-06-12 14:25:22 -0400

    gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductTweeter::Application.config.secret_token = '16f0d65e56256990cceeebb711b7439f71ec0a33e36453566e919eae5e8586468844f6679bac71cc2e1b0f975a13477fb68be0ba7d6ca0c59fa70d018fdc43df'
